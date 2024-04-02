fec35c5c9bfc95a32270af2d1febe7f1400da90e
adiaz23 <aldiaz0223@gmail.com>
2024-04-01 20:02:06 -0400

    Refactor test for get characters

--- src/tests/API/getCharacters.spec.js
+++ src/tests/API/getCharacters.spec.js
@@ -13,20 +13,18 @@ test.describe("Fetch characters from Marvel API", async()=>{
     });
 
     test("Fetch All Characters", async()=>{
-        let response = await apiRequest.get(`/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`);
+        let response = await apiRequest.get(`/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}&limit=100`);
         await expect(response).toBeOK();
     });
 
     test("Fetch more than 100 characters", async()=>{
-        let request = await apiRequest.get(`/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}&limit=101`);
-        let response = await request.json();
+        let response = await (await apiRequest.get(`/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}&limit=101`)).json();
         expect(response.code).toBe(409);
         expect(response.status).toBe("You may not request more than 100 items.");
     });
 
-    test("Fetch zero character", async()=>{
-        let request = await apiRequest.get(`/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}&limit=0`);
-        let response = await request.json();
+    test("Fetch less than 1 character", async()=>{
+        let response = await (await apiRequest.get(`/v1/public/characters?ts=${timeStamp}&apikey=${publicKey}&hash=${hash}&limit=0`)).json();
         expect(response.code).toBe(409);
         expect(response.status).toBe("You must pass an integer limit greater than 0.");
     });
